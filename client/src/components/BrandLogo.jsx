87785c892b2bb2f440d3d417c2d3c5206477ca90
Daryl Daruwala <55801366+Vohala@users.noreply.github.com>
2026-02-24 00:26:36 +0530

    Delete client directory

--- client/src/components/BrandLogo.jsx
+++
@@ -1,8 +0,0 @@
-export default function BrandLogo({ size=40 }) {
-  return (
-    <div className="flex items-center gap-2">
-      <img src="/vohala-logo.svg" width={size*2} height={size} alt="Vohala" />
-      <span className="font-extrabold text-xl">Vohala Social</span>
-    </div>
-  )
-}
